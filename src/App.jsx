2bae7474ba2d4910c8a2b1b843bf5bd55d3ec0df
Dev Js <alienwaredev369@outlook.com>
2026-09-05 09:17:06 -0300

    Arquivos do projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <input type="text" placeholder="O que tenho para fazer?" />
+    </>
+  )
+}
+
+export default App
