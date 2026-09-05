e475546e5a0576638ba584b3d62abbba5a5ce75d
Dev Js <alienwaredev369@outlook.com>
2026-09-05 13:31:32 -0300

    Criação das funções dos botões

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
+
+  function inputMudou(event) {
+    console.log(event.target.value)
+  }
+
+  function cliqueiNoBotao() {
+    console.log('cliquei no botão')
+  }
 
   return (
     <>
-      <input type="text" placeholder="O que tenho para fazer?" />
+      <input onChange={inputMudou} type="text" placeholder="O que tenho para fazer?" />
+      <button onClick={cliqueiNoBotao}>Adicionar</button>
+
+      <ul>
+        <li>Ir à academia</li>
+        <li>Estudar React</li>
+        <li>Fazer exercícios</li>
+      </ul>
     </>
   )
 }
