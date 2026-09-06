d9aecd07b48ffe18df26f5e12cc0b3411600c17b
Dev Js <alienwaredev369@outlook.com>
2026-09-06 16:55:02 -0300

    Usando o método push() para adicionar um elemento ao array

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { v4 as uuidv4 } from 'uuid';
-
+//biblioteca para gerar id's aleatórios
 function App() {
 
   const list = [{ id: uuidv4(), task: "Ir à academia" }, { id: uuidv4(), task: "Estudar React" }, { id: uuidv4(), task: "Fazer exercícios" }]
 
   function inputMudou(event) {
     console.log(event.target.value)
+    list.push({ id: uuidv4(), task: event.target.value })
+    console.log(list)
   }
 
   function cliqueiNoBotao() {
